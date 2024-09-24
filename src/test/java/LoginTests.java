ae86349c67862b09533ce3038a9a1f8db9e4549f
daiyrisaev <isaevdaiyr@gmail.com>
2024-09-23 20:13:07 -0500

    first commit

---
+++ src/test/java/LoginTests.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoginTests {
+
+    public void loginTest(){
+        Assert.assertTrue(2<10);
+
+    }
+    @Test
+    public void loginTest2(){
+        System.out.println("erlan".contains("e"));
+
+    }
+
+}
